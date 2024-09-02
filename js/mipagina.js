98892916d3062ea67bc1632504d16504a636a1a0
Frankito1909 <Franconahueljoseveppo@gmail.com>
2024-09-02 19:06:38 -0300

    Initial commit

---
+++ js/mipagina.js
@@ -0,0 +1,17 @@
+
+document.addEventListener('DOMContentLoaded', () => {
+    const boton = document.getElementById('card1');
+    
+    boton.addEventListener('click', () => {
+        // Redirige al usuario a la página deseada
+        window.location.href = "/acercade.html";
+    });
+});
+document.addEventListener('DOMContentLoaded', () => {
+            const boton = document.getElementById('card1');
+            
+            boton.addEventListener('click', () => {
+                // Redirige al usuario a la página deseada
+                window.location.href = "/acercade.html";
+            });
+        });
